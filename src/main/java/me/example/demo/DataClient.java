0c01b2eaf9723fcf8a56abb488bfa97409f36075
JEONG-BD <dev.byeongdu@gmail.com>
2025-08-28 20:22:28 +0900

    order, config

---
+++ src/main/java/me/example/demo/DataClient.java
@@ -0,0 +1,31 @@
+package me.example.demo;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import me.example.demo.order.Order;
+import me.example.demo.payment.Payment;
+import me.example.demo.payment.PaymentService;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.math.BigDecimal;
+
+public class DataClient {
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataCofig.class);
+        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Order order = new Order("100", BigDecimal.TEN);
+        System.out.println(order);
+        em.persist(order);
+        System.out.println(order);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
